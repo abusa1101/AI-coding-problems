f012d50e07155ed295e3454ef579c9abc5c3b260
Anviksha Busa <abusa@umich.edu>
2020-04-05 14:01:18 -0400

    fixing the alg to work with my newly formatted input

--- HW/Project4/helper_funcs.py
+++ HW/Project4/helper_funcs.py
@@ -1,78 +1,86 @@
 import csv
-import inference as inf
-from collections import Counter
+# import inference as inf
 
 def read_input_file(file_name):
     file = csv.reader(open(file_name, 'r'), skipinitialspace='True')
-    in_list = list(file)
-    X = in_list[1]
+    input_list = list(file)
+    X = input_list[1]
     e = {}
-    for evidence in in_list[3]:
+    for evidence in input_list[3]:
         e[evidence[0]] = evidence[2]
     return (X,e)
 
 def read_bn_file(file_name):
-    f = open(file_name) # Open file on read mode
+    f = open(file_name)
     lines = f.read().split("\n") # Create a list containing all lines
-    f.close() # Close file
-
-    pv_idx = -1
-    for i, item in enumerate(lines):
-        if item == '% Probability values':
-            pv_idx = i #idx where Probabilities start next
+    f.close()
 
     vars = []
     for var in lines[1]: #create a bn dict with empty entries for [parents] and {probabilities}
         if var != ' ' and var != ',':
             vars.append(var)
 
-    # for i, item in enumerate(lines): #add [parents] to bn from graph edges
-    #     if i > 2 and i < pv_idx:
-    #         bn[item[3]][0].append(item[0])
+    pv_idx = -1 #'% Probability Values' comment index (end of graph edges and start of P values)
+    for i, item in enumerate(lines):
+        if item == '% Probability values':
+            pv_idx = i #idx where Probabilities start next
 
     p_list = [[],[],[],[],[],[]] #Node, Parent1, T/F1, Parent2, T/F2, Prob(Node | Parents and T/F)
     for i, item in enumerate(lines):
         if i > pv_idx and item:
-            p_list[0].append(item[2])
-            if item[6].isalpha():
+            p_list[0].append(item[2]) #assign Node
+            if item[6].isalpha(): #assign Parent1
                 p_list[1].append(item[6])
             else:
-                p_list[1].append([])
+                p_list[1].append([]) #assign null if no Parent1
             if item[8].isalpha():
-                p_list[2].append(item[8])
+                p_list[2].append(item[8]) #assign Parent1 boolean (T/F)
             else:
-                p_list[2].append([])
+                p_list[2].append([]) #assign null if no Parent1
             if len(item) > 10 and item[10].isalpha():
-                p_list[3].append(item[10])
+                p_list[3].append(item[10]) #assign Parent2
             else:
-                p_list[3].append([])
+                p_list[3].append([]) #assign null if no Parent2
             if len(item) > 10 and item[12].isalpha():
-                p_list[4].append(item[12])
+                p_list[4].append(item[12]) #assign Parent2 boolean (T/F)
             else:
-                p_list[4].append([])
-            p_list[-1].append('0.' + item[-1])
-    bn2 = {}
-    prob = {}
+                p_list[4].append([]) #assign null if no Parent2
+            p_list[-1].append(float('0.' + item[-1])) #assign probabilities wrt nodes, parents and boolean values
+    bn = {}
     for i in range(len(p_list[0])): #create a bn dict with empty entries for [parents] and {probabilities}
         if not p_list[2][i]:
             p_list[2][i] = 'None'
         if not p_list[4][i]:
             p_list[4][i] = 'None'
-        if p_list[0][i] not in bn2.keys():
-            bn2[p_list[0][i]] = [[p_list[1][i], p_list[3][i]], {(p_list[2][i], p_list[4][i]):p_list[5][i]}]
+        if p_list[0][i] not in bn.keys():
+            bn[p_list[0][i]] = [[p_list[1][i], p_list[3][i]], {(p_list[2][i], p_list[4][i]):p_list[5][i]}]
         else:
-            temp_var = p_list[0][i]
-            temp_dict1 = bn2[p_list[0][i]][1]
-            temp_dict2 = {(p_list[2][i], p_list[4][i]):p_list[5][i]}
-            temp_dict2.update(temp_dict1)
-            del bn2[p_list[0][i]]
-            bn2[p_list[0][i]] = [[p_list[1][i], p_list[3][i]], temp_dict2]
-    return (vars, bn2)
+            rep_var = p_list[0][i] #repeated node
+            prev_prob_entry = bn[rep_var][1] #previous probability entry
+            merged_prob_entry = {(p_list[2][i], p_list[4][i]):p_list[5][i]} #new probability entry
+            merged_prob_entry.update(prev_prob_entry) #merge previous and new probability entries
+            del bn[rep_var] #delete previous prob entry
+            bn[rep_var] = [[p_list[1][i], p_list[3][i]], merged_prob_entry] #replace with updated merged entry
+    return (vars, bn)
 
 (X, e) = read_input_file("input2.txt")
-vars, bn = read_bn_file("bn2.txt") #vars,bn
+(vars, bn) = read_bn_file("bn2.txt")
+#
+# bn = {'B':[[[],[]],{(None, None):.001}],
+#       'E':[[[], []],{(None,None):.002}],
+#       'A':[['B','E'],
+#                {(False,False):.001,(False,True):.29,
+#                 (True,False):.94,(True,True):.95}],
+#       'J':[['A'],
+#                    {(False,None):.05,(True,None):.90}],
+#       'M':[['A'],
+#                    {(False,None):.01,(True,None):.70}]}
+# vars = ['M','J','A','B','E']
 
-print(type(vars))
+print(X)
+print(e)
+print(vars)
 print(bn)
+
 # print(inf.enumerationAsk(X[0],e,bn,vars))
 # enumerationAsk(X, e, bn,vars):
--- HW/Project4/inference.py
+++ HW/Project4/inference.py
@@ -3,25 +3,43 @@ import copy as cp
 def prob(Y, e, bn):
     parents = bn[Y][0]
     if not parents:
-        P = bn[Y][1][None] #CHANGE THIS WRT INPUT
+        P = bn[Y][1][('None','None')]
     else: #Parents exist
-        p_vals = tuple(e[p] for p in parents)  #value of parents(Y)
+        # print(Y)
+        # print(bn[Y])
+        print(parents)
+        print(e)
+        p_vals_list = []
+        for parent in parents:
+            p_vals_list.append(str(e[parent]))
+        if len(p_vals_list) != 2:
+            p_vals_list.append('None')
+        p_vals = tuple(p_vals_list)
+        print(bn[Y][1])
+        print(p_vals)
         P = bn[Y][1][p_vals] #Probability of Parent(Y) values
     return P if e[Y] else (1.0 - P)
 
-def enumerateAll(vars, e,bn):
+def enumerateAll(vars, e, bn):
     if not vars:
         return 1.0
     Y = vars.pop()
+    # print(Y)
+    # print(e)
     if Y in e:
+        # print("in")
         query = prob(Y,e,bn) * enumerateAll(vars,e,bn)
         vars.append(Y)
         return query
     else:
+        # print('else')
         query_sum = 0
         e_cpy = cp.deepcopy(e)
+        # print(e_cpy)
         for boolval in [False, True]:
+            # print(Y)
             e_cpy[Y] = boolval
+            print(e_cpy)
             query_sum += prob(Y,e_cpy,bn) * enumerateAll(vars,e_cpy,bn)
         vars.append(Y)
         return query_sum
@@ -37,18 +55,19 @@ def enumerationAsk(X, e, bn,vars):
     for xi in [False,True]: #because boolean values
         e_cpy = cp.deepcopy(e)
         e_cpy[X] = xi
-        Q[xi] = enumerateAll(vars,e_cpy,bn)
-    return normalize(Q)
+        Q[xi] = enumerateAll(vars, e_cpy, bn)
+    return normalize(Q)# Run Program for given input
 
-# # Run Program for given input
-# bn = {'B':[[],{None:.001}],
-#       'E':[[],{None:.002}],
-#       'A':[['B','E'],
-#                {(False,False):.001,(False,True):.29,
-#                 (True,False):.94,(True,True):.95}],
-#       'J':[['A'],
-#                    {(False,):.05,(True,):.90}],
-#       'M':[['A'],
-#                    {(False,):.01,(True,):.70}]}
-# vars = ['M','J','A','B','E']
-# print(enumerationAsk('B',{'J':True,'M':True},bn,vars))
+
+# Run Program for given input
+bn = {'B':[[],{('None','None'):.001}],
+      'E':[[],{('None','None'):.002}],
+      'A':[['B','E'],
+               {('False','False'):.001,('False','True'):.29,
+                ('True','False'):.94,('True','True'):.95}],
+      'J':[['A'],
+                   {('False','None'):.05,('True','None'):.90}],
+      'M':[['A'],
+                   {('False','None'):.01,('True','None'):.70}]}
+vars = ['M','J','A','B','E']
+print(enumerationAsk('B',{'J':'True','M':'True'}, bn, vars))
